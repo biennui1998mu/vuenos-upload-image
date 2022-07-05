203b21b2f6a26517ee02f1b4b6f900cf2548dbb9
sonndh.safin <42411725+biennui1998mu@users.noreply.github.com>
2022-07-05 16:55:15 +0700

    update

--- dev/serve.vue
+++ dev/serve.vue
@@ -1,16 +1,27 @@
 <template>
   <div id="app">
-    <VuenosUploadImage />
+    <vuenos-upload-image :type="'upload'"
+                         @getImgFileList="getImgFileList">
+    </vuenos-upload-image>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue';
+import {defineComponent} from 'vue';
 import VuenosUploadImage from '@/vuenos-upload-image.vue';
 
 export default defineComponent({
   name: 'ServeDev',
   components: {
     VuenosUploadImage
+  },
+  setup() {
+    const getImgFileList = (val: any) => {
+      console.log(val)
+    }
+
+    return{
+      getImgFileList
+    }
   }
 });
 </script>
